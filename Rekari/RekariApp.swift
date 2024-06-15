b76a36064526cc9e65ec0fbd782b8a4042fb47ea
Takayoshi Katsuki <dev@annotunzdy.info>
2024-06-15 16:24:00 +0900

    Initial Commit

---
+++ Rekari/RekariApp.swift
@@ -0,0 +1,17 @@
+//
+//  RekariApp.swift
+//  Rekari
+//
+//  Created by development on 2024/06/15.
+//
+
+import SwiftUI
+
+@main
+struct RekariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
